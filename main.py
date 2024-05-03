9b4e04c8c2912a9c9df526e596a9be1d9cd6f0ca
tamersenan01 <33778801-tamersenan01@users.noreply.replit.com>
2024-05-03 00:53:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+################################
+# File: main.py                #
+# Purpose: File that trains    #
+# and tests the model          #
+################################
+import pandas as pd
+import model
+
+## import the datasets
+
+
+## steralize the dataset
+
+## train the model
+
+## test the model
+
+## show results
---
+++ model.py
@@ -0,0 +1,13 @@
+################################
+# File: model.py               #
+# Purpose: Model that is used  #
+# to classify formality.       #
+################################
+class Model:
+  def __init__(self) -> None:
+    pass
+
+  ## define any new functions here.
+  
+  def __str__(self) -> str:
+    return "This is a model"
