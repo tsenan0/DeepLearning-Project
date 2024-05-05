f9d57169643d2ec72807e800c81f3851b0a1283c
braksy <leosegura628@gmail.com>
2024-05-05 15:59:36 -0700

    keras model, accuracy

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import pandas as pd
 import numpy as np
 from gensim.models import Word2Vec
+from sklearn.metrics import confusion_matrix, classification_report
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Embedding, Flatten, Dense
@@ -25,23 +26,45 @@ data_test['avg_score'] = data_test['avg_score'].apply(lambda x: 1 if x>=0 else 0
 
 # make word embeddings
 model = Word2Vec(data_train['sentence'], min_count = 1)
-weights = model.wv.vectors
-vocab_len, embed_len = weights.shape
-
 data_train['sentence'] = data_train['sentence'].apply(lambda x: [model.wv.get_vector(word) for word in x])
+data_test['sentence'] = data_test['sentence'].apply(lambda x: [model.wv.get_vector(word) for word in x if word in model.wv.key_to_index])
 
 ## train the model
 maxlen = max(data_train['sentence'].apply(len))
-padded_sequences = pad_sequences(data_train['sentence'], padding='post', maxlen=maxlen)
+padded_sequences_train = pad_sequences(data_train['sentence'], padding='post', maxlen=maxlen)
+padded_sequences_test = pad_sequences(data_test['sentence'], padding='post', maxlen=maxlen)
 
-data_train['sentence'] = padded_sequences.tolist()
-
-x_train = np.stack(data_train['sentence'].values)
+x_train = np.stack(padded_sequences_train)
 y_train = data_train['avg_score'].values
 
-print(x_train)
-print(y_train)
+x_test = np.stack(padded_sequences_test)
+y_test = data_test['avg_score'].values
 
-## test the model
+# Create a Keras model
+model = Sequential()
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
 
-## show results
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# evaluate the model
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test Accuracy: {accuracy*100}')
+
+# get the model's predictions on the test set
+y_pred = model.predict(x_test)
+y_pred = np.round(y_pred)  #round the predictions to 0 or 1
+
+# generate a confusion matrix, testing our classifier to see how many right and wrong predictions it made
+cm = confusion_matrix(y_test, y_pred)
+print('Confusion Matrix:')
+print(cm)
+
+# generate a classification report, report of each precision, recall, f1-score and support
+report = classification_report(y_test, y_pred)
+print('Classification Report:')
+print(report)
